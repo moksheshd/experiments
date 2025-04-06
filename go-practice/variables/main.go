31dbae0a1f5792731d964bad87d6c14b1921ba6d
Mokshesh Dafariya <mokshesh@outlook.com>
2025-04-07 00:36:50 +0530

    Add variables example demonstrating declaration methods
    This commit adds examples of Go variable declarations including:
    - Zero-value initialization (int, bool)
    - Explicit type declaration with initialization
    - Short variable declaration using := operator

---
+++ go-practice/variables/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Variables example")
+	var num int
+	fmt.Println(num)
+
+	var decision bool
+	fmt.Println(decision)
+
+	var age int = 23
+	fmt.Println(age)
+
+	var isDeleted bool = true
+	fmt.Println(isDeleted)
+
+	count := 4
+	fmt.Println(count)
+
+}
